70015d4897a81ad693634a46c94bfed4f236bc99
xiaotai2016 <bowens@adbsystems.com.au>
2021-05-02 16:47:19 +1000

    todolist without redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
+import React, {useState} from 'react'
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [list, setList] = useState([])
+  const handleInput = (e)=>{
+    setInputValue (e.target.value); 
+  }
+  const handListChange = ()=>{
+    setList([...list,inputValue])
+    setInputValue('')
+  }
+  const handDelete= (index)=>{
+    var newlist = list;
+    newlist.splice(index,1)
+    setList([...newlist])
+  }
   return (
-    <div className="App">
-      Hello World
+    <div>
+    <input value={inputValue} onChange= {handleInput} />
+    <button onClick={handListChange}>Submit</button>
+    <ul>
+      {
+        list.map((item, index)=> (<li key={item} onClick={()=>{handDelete(index)}}>{item}</li>))
+      }
+    </ul>
     </div>
   );
+
+
+
 }
 
 export default App;
